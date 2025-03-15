0915dd22f991631b0e37bd0b8065b21ce002ba54
HassanAbdelbaset <hassanelfalt60@gmail.com>
2025-03-15 15:11:44 +0200

    refactor: adjust naming for fragment_pager

--- app/src/main/java/com/ar/salata/ui/fragments/MainCategoryPagerFragment.java
+++ app/src/main/java/com/ar/salata/ui/fragments/MainCategoryPagerFragment.java
@@ -47,7 +47,7 @@ public class MainCategoryPagerFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.fragment_pager, container, false);
+		View view = inflater.inflate(R.layout.fragment_main_category_pager, container, false);
 		
 		if (getActivity() instanceof HomeActivity) {
 			view.findViewById(R.id.appbar_pager_fragment).setVisibility(View.VISIBLE);
